507d85c7ba8710441bd6d6b1134bdbc62f94cdb0
mark-1701 <vini.muralles@gmail.com>
2024-10-10 19:38:13 -0600

    add (product): api routes

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,19 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+// ruta publica para product
+Route::get('product', [UserController::class, 'index']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     // * rutas login
     Route::get('user-profile', [AuthController::class, 'userProfile']);
     Route::get('logout', [AuthController::class, 'logout']);
     Route::resource('user', UserController::class);
+    Route::resource('product', ProductController::class)->except(['index']);;
 });
